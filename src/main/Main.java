9add3a719262d0112877e074d1f98f4729c9279c
JustCoffeeChris <47278899+JustCoffeeChris@users.noreply.github.com>
2020-07-24 23:04:31 +0200

    Farben angepasst und ein wenig cleanup

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,43 +1,33 @@
 package main;
 
 import java.util.ArrayList;
-
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import style.Bundestag;
 
-public class Main extends Application implements Runnable {
+public class Main extends Application {
 
 	private Button createChart = new Button("Chart erstellen");
 	private Button checkButton = new Button("Aktualisieren");
-	private ArrayList<Double> content = new ArrayList<Double>();
+	private ArrayList<Double> content;
 	private ArrayList<TextField> listOfTextFields = new ArrayList<TextField>();
 	private Label remainSeats = new Label();
-	Double remain;
-	
-	private void createButtonEvent() {
+	private Double remain;
 
+	private void createButtonEvent() {
+		content = new ArrayList<Double>();
 		createChart.setOnAction(e -> {
 
 			for (int i = 0; i < listOfTextFields.size(); i++) {
-				System.out.println(listOfTextFields.get(i).getText());
-			}
-
-			for (int i = 0; i < listOfTextFields.size(); i++) {
 				content.add(Double.parseDouble(listOfTextFields.get(i).getText()));
-				System.err.println(content.get(i));
+
 			}
 
 			Bundestag bundestag = new Bundestag(content);
@@ -48,28 +38,17 @@ public class Main extends Application implements Runnable {
 
 	private void checkButtonEvent() {
 
-		
 		checkButton.setOnAction(e -> {
 			remain = 100.0;
 			content = new ArrayList<Double>();
-			for (int i = 0; i < listOfTextFields.size(); i++) {
-				System.out.println(listOfTextFields.get(i).getText());
-			}
 
 			for (int i = 0; i < listOfTextFields.size(); i++) {
-				content.add( Double.parseDouble(listOfTextFields.get(i).getText()) );
-				System.err.println(content.get(i));
-				remain = remain - content.get(i);
+				content.add(Double.parseDouble(listOfTextFields.get(i).getText()));
+				remain -= content.get(i);
 			}
 
 			remainSeats.setText(remain.toString());
 		});
-
-	}
-
-	public static void main(String[] args) {
-
-		launch(args);
 
 	}
 
@@ -80,11 +59,11 @@ public class Main extends Application implements Runnable {
 
 		VBox root = null;
 
-		listOfTextFields.add(new TextField("SPD"));
-		listOfTextFields.add(new TextField("Union"));
-		listOfTextFields.add(new TextField("FDP"));
-		listOfTextFields.add(new TextField("B90/Gruene"));
 		listOfTextFields.add(new TextField("Linke"));
+		listOfTextFields.add(new TextField("B90/Gruene"));
+		listOfTextFields.add(new TextField("SPD"));
+		listOfTextFields.add(new TextField("FDP"));
+		listOfTextFields.add(new TextField("Union"));
 		listOfTextFields.add(new TextField("AfD"));
 
 		root = new VBox(listOfTextFields.size() + 2);
@@ -95,22 +74,21 @@ public class Main extends Application implements Runnable {
 		root.getChildren().add(createChart);
 		root.getChildren().add(remainSeats);
 		root.getChildren().add(checkButton);
-		
+
 		Scene scene = new Scene(new Group());
 		scene = new Scene(new ScrollPane(root), 250, listOfTextFields.get(0).getHeight() * listOfTextFields.size() - 1);
 
 		createButtonEvent();
 		checkButtonEvent();
-		
+
 		stage.setScene(scene);
 		stage.show();
 
 	}
 
-	@Override
-	public void run() {
-		createButtonEvent();
+	public static void main(String[] args) {
+
+		launch(args);
 
 	}
-
 }
--- src/style/Bundestag.java
+++ src/style/Bundestag.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
 
-public class Bundestag implements Runnable{
+public class Bundestag{
 
 	private ArrayList<Double> content = new ArrayList<Double>();
 
@@ -23,14 +23,24 @@ public class Bundestag implements Runnable{
 	public PieChart getPieChart() {
 
 		ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(new PieChart.Data("", 50),
-				new PieChart.Data("SPD", this.content.get(0)), new PieChart.Data("Union", this.content.get(1)),
-				new PieChart.Data("FDP", this.content.get(2)), new PieChart.Data("Grune", this.content.get(3)),
-				new PieChart.Data("Linke", this.content.get(4)), new PieChart.Data("AfD", this.content.get(5)));
+				new PieChart.Data("Linke", this.content.get(0)), 
+				new PieChart.Data("B90/Gruene", this.content.get(1)),
+				new PieChart.Data("SPD", this.content.get(2)), 
+				new PieChart.Data("FDP", this.content.get(3)),
+				new PieChart.Data("Union", this.content.get(4)), 
+				new PieChart.Data("AfD", this.content.get(5)));
 		
 		final PieChart chart = new PieChart(pieChartData);
 		chart.setTitle("Bundestag Sitzverteilung");
+		
 		pieChartData.get(0).getNode().setStyle("-fx-pie-color: #ffffff;");
-
+		pieChartData.get(1).getNode().setStyle("-fx-pie-color: #cb99c9;");
+		pieChartData.get(2).getNode().setStyle("-fx-pie-color: #46962b;");
+		pieChartData.get(3).getNode().setStyle("-fx-pie-color: #E3000F;");
+		pieChartData.get(4).getNode().setStyle("-fx-pie-color: #ffed00;");
+		pieChartData.get(5).getNode().setStyle("-fx-pie-color: #000000;");
+		pieChartData.get(6).getNode().setStyle("-fx-pie-color: #009ee0;");
+		
 		return chart;
 
 	}
@@ -45,9 +55,6 @@ public class Bundestag implements Runnable{
 		newStage.show();
 	}
 
-	@Override
-	public void run() {
-		showGraph();
-	}
+
 
 }
